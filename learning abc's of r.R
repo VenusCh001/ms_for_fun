22ac2e7f62acd465c945925a826456888433dde8
VenusCh001 <venus222btcse22@igdtuw.ac.in>
2024-11-30 16:57:26 +0530

    learned basic

--- learning abc's of r.R
+++ learning abc's of r.R
@@ -1,2 +1,16 @@
+#variables making
 x<-55
-print(x)
+y<-44
+
+#make vector
+firstvector=c("a","b","c","d")
+secondvector=c("alpha","beta","gamma","theta")
+
+#creating matrix
+mymatrix=matrix(nrow=3,ncol=3,byrow=TRUE,data=c(1,2,3,4,5,6,7,8,9))
+
+#creating dataframe
+my_df=data.frame(alphabet=firstvector,namesOfPeeps=secondvector)
+my_df$score=c(100,200,300,400)
+my_df[1,]
+
